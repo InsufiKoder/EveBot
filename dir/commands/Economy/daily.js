e1aed22be9065c9c932faa9bf9d3eb52fc65e32c
InsufiKoder <realomrrkn@gmail.com>
2022-01-15 22:12:48 +0300

    Add embeds to daily.js

--- dir/commands/Economy/daily.js
+++ dir/commands/Economy/daily.js
@@ -1,4 +1,4 @@
-const { Message, Client } = require("discord.js");
+const { Message, Client, MessageEmbed } = require("discord.js");
 const economy = require("discord-bot-eco");
 const cooldown = new Set();
 
@@ -16,18 +16,32 @@ module.exports = {
     const userid = message.author.id;
     const amount = 2500;
 
+    // Embeds start
+    const cooldownembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Cooldown")
+      .setDescription(
+        "You should wait 24 hours before using this command again."
+      )
+      .setTimestamp();
+
+    const replyembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Success!")
+      .setDescription(`${amount} added to your wallet!`)
+      .setTimestamp();
+    // Embeds end
+
     setTimeout(() => {
       cooldown.delete(message.author.id);
     }, 86400000);
 
     if (cooldown.has(message.author.id)) {
-      message.reply(
-        "You should wait 24 hours before using this command again."
-      );
+      message.reply({ embeds: cooldownembed });
     } else {
       await economy.daily(userid, amount);
 
-      message.reply(`${amount} added to your wallet!`);
+      message.reply({ embeds: replyembed });
       cooldown.add(message.author.id);
     }
   },
