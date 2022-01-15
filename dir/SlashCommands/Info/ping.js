ef256f807fae7a819e71466cb0a67d4cddc026cd
InsufiKoder <87572099+InsufiKoder@users.noreply.github.com>
2022-01-16 01:40:22 +0300

    Added embeds to slash commands

--- dir/SlashCommands/Info/ping.js
+++ dir/SlashCommands/Info/ping.js
@@ -1,16 +1,22 @@
-const { Client, CommandInteraction } = require("discord.js");
+const { Client, CommandInteraction, MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: "ping",
-    description: "returns websocket ping",
-    type: 'CHAT_INPUT',
-    /**
-     *
-     * @param {Client} client
-     * @param {CommandInteraction} interaction
-     * @param {String[]} args
-     */
-    run: async (client, interaction, args) => {
-        interaction.followUp({ content: `${client.ws.ping}ms!` });
-    },
+  name: "ping",
+  description: "returns websocket ping",
+  type: "CHAT_INPUT",
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   * @param {String[]} args
+   */
+  run: async (client, interaction, args) => {
+    const replyembed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setTitle("Ping")
+      .setDescription(`${client.ws.ping}ms!`)
+      .setTimestamp();
+
+    interaction.followUp({ embeds: [replyembed] });
+  },
 };
