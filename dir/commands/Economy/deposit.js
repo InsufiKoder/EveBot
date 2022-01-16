24d362b04b4ce34098d0fae38546631f050245b0
InsufiKoder <realomrrkn@gmail.com>
2022-01-16 15:33:21 +0300

    değiştirildi:         dir/commands/Economy/deposit.js 	değiştirildi:         dir/events/ready.js

--- dir/commands/Economy/deposit.js
+++ dir/commands/Economy/deposit.js
@@ -14,28 +14,35 @@ module.exports = {
   run: async (client, message, args) => {
     try {
       const userid = message.author.id;
-      const convert = parseInt(args);
+
+      if (args[0] == 0)
+        return message.reply("Please insert an amount more than 0.");
+
+      if (isNaN(args[0])) return message.reply("Argument must be a number.");
+
+      const amount = parseInt(args[0]);
 
       // Embeds start
       const replyembed = new MessageEmbed()
         .setColor("RANDOM")
         .setTitle("Success!")
-        .setDescription(`Deposited ${convert}!`)
+        .setDescription(`Deposited ${amount}!`)
         .setTimestamp();
       // Embeds end
 
-      await economy.deposit(userid, convert);
+      await economy.deposit(userid, amount);
       message.reply({ embeds: [replyembed] });
     } catch (err) {
       // Embeds start
       const errorembed = new MessageEmbed()
         .setColor("RANDOM")
         .setTitle("Error")
-        .setDescription("Please insert an amount greater than 0.")
+        .setDescription("An error occured. Please try again.")
         .setTimestamp();
       // Embeds end
 
       message.reply({ embeds: [errorembed] });
+      console.log(err);
     }
   },
 };
