2c53f68c0ee22ae96f663eaf8c4480e6cd9d3cbf
InsufiKoder <realomrrkn@gmail.com>
2022-02-15 21:53:44 +0300

    Add uselessweb

---
+++ dir/commands/Fun/uselessweb.js
@@ -0,0 +1,22 @@
+const { Message, Client } = require("discord.js");
+const webList = require("../../uselessweblist.js").varToExport;
+
+module.exports = {
+  name: "uselessweb",
+  description: "Returns a website from https://theuselessweb.com",
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    try {
+      const randomWeb = webList[Math.floor(Math.random() * webList.length)];
+
+      message.reply(`${randomWeb}`);
+    } catch (err) {
+      message.reply("An error occured. Please try again.");
+    }
+  },
+};
