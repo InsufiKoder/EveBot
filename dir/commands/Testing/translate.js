da91fa28ef1b42882f62ee271ab0f8f23cb9bdf4
InsufiKoder <realomrrkn@gmail.com>
2022-02-19 23:11:59 +0300

    new file:   dir/commands/Testing/translate.js 	modified:   package-lock.json 	modified:   package.json

---
+++ dir/commands/Testing/translate.js
@@ -0,0 +1,42 @@
+const { Message, Client, MessageEmbed } = require("discord.js");
+require("dotenv").config();
+const translate = require("translate-google");
+
+module.exports = {
+  name: "translate",
+  description: "Translates. usage: -translate tr Merhaba.",
+  /**
+   *
+   * @param {Client} client
+   * @param {Message} message
+   * @param {String[]} args
+   */
+  run: async (client, message, args) => {
+    try {
+      const language = args[0];
+      const input = args.slice(1).join(" ");
+
+      translate(input, { to: language })
+        .then((res) => {
+          const replyEmbed = new MessageEmbed()
+            .setAuthor(
+              message.author.tag,
+              message.author.displayAvatarURL({ dyanmic: true })
+            )
+            .setTitle(`Translated to: ${language}`)
+            .setColor("RANDOM")
+            .setDescription(res)
+            .setTimestamp();
+
+          message.reply({ embeds: [replyEmbed] });
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    } catch (err) {
+      message.reply(
+        "An error occured. Please make sure you gave the bot a language to translate to."
+      );
+    }
+  },
+};
