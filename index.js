e403f7f59750eaff6fc6104830a6005d5ad3e0ef
InsufiKoder <realomrrkn@gmail.com>
2022-02-19 21:32:45 +0300

    edit index

--- index.js
+++ index.js
@@ -15,67 +15,6 @@ client.slashCommands = new Collection();
 client.config = require("./config.json");
 client.emotes = client.config.emoji;
 
-const { SpotifyPlugin } = require("@distube/spotify");
-client.distube = new DisTube(client, {
-  leaveOnStop: false,
-  emitNewSongOnly: true,
-  emitAddSongWhenCreatingQueue: false,
-  emitAddListWhenCreatingQueue: false,
-  plugins: [
-    new SpotifyPlugin({
-      emitEventsAfterFetching: true,
-    }),
-  ],
-});
-
-const status = (queue) =>
-  `Volume: \`${queue.volume}%\` | Filter: \`${
-    queue.filters.join(", ") || "Off"
-  }\` | Loop: \`${
-    queue.repeatMode
-      ? queue.repeatMode === 2
-        ? "All Queue"
-        : "This Song"
-      : "Off"
-  }\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
-client.distube
-  .on("playSong", (queue, song) =>
-    queue.textChannel.send(
-      `${client.emotes.play} | Playing \`${song.name}\` - \`${
-        song.formattedDuration
-      }\`\nRequested by: ${song.user}\n${status(queue)}`
-    )
-  )
-  .on("addSong", (queue, song) =>
-    queue.textChannel.send(
-      `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
-    )
-  )
-  .on("addList", (queue, playlist) =>
-    queue.textChannel.send(
-      `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
-        playlist.songs.length
-      } songs) to queue\n${status(queue)}`
-    )
-  )
-  .on("error", (channel, e) => {
-    channel.send(
-      `${client.emotes.error} | An error encountered: ${e
-        .toString()
-        .slice(0, 1974)}`
-    );
-    console.error(e);
-  })
-  .on("empty", (channel) =>
-    channel.send("Voice channel is empty! Leaving the channel...")
-  )
-  .on("searchNoResult", (message, query) =>
-    message.channel.send(
-      `${client.emotes.error} | No result found for \`${query}\`!`
-    )
-  )
-  .on("finish", (queue) => queue.textChannel.send("Finished!"));
-
 require("./dir/handler")(client);
 
 client.login(process.env.TOKEN);
