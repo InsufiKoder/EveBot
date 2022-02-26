cd614ed06475fe8d44610e180087571946675c7d
InsufiKoder <realomrrkn@gmail.com>
2022-02-26 15:40:44 +0300

    new file:   dir/SlashCommands/Misc/wolfram.js 	deleted:    dir/commands/Misc/wolfram.js

---
+++ dir/SlashCommands/Misc/wolfram.js
@@ -0,0 +1,30 @@
+const { Client, MessageEmbed, CommandInteraction } = require("discord.js");
+require("dotenv").config();
+const WolframAlphaAPI = require("wolfram-alpha-node");
+const waApi = WolframAlphaAPI(process.env.WOLFRAM_APPID);
+
+module.exports = {
+  name: "wolfram",
+  description: "Searches user input in wolfram.",
+  options: [
+    {
+      name: "query",
+      description: "The thing to search on wolfram",
+      type: "STRING",
+      required: "true",
+    },
+  ],
+  /**
+   *
+   * @param {Client} client
+   * @param {CommandInteraction} interaction
+   */
+  run: async (client, interaction) => {
+    const input = interaction.options.getString("query");
+    const response = await waApi.getSimple(input);
+
+    const sfbuff = new Buffer.from(response.split(",")[1], "base64");
+
+    interaction.followUp({ files: [{ attachment: sfbuff }] });
+  },
+};
